8a3fa510fa84cc932bb2e07d061d542b18994e70
Fabiano Gonzalez <fabianolgonzales@gmail.com>
2016-07-20 10:06:56 +0200

    user-model test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719142428) do
+ActiveRecord::Schema.define(version: 20160719144438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,22 @@ ActiveRecord::Schema.define(version: 20160719142428) do
   end
 
   add_index "items", ["user_id"], name: "index_items_on_user_id", using: :btree
+
+  create_table "user_details", force: :cascade do |t|
+    t.string   "username"
+    t.string   "name_first"
+    t.string   "name_last"
+    t.string   "zipcode"
+    t.string   "address_st"
+    t.integer  "address_hno"
+    t.string   "address_cpm"
+    t.string   "city"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "user_details", ["user_id"], name: "index_user_details_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -48,4 +64,5 @@ ActiveRecord::Schema.define(version: 20160719142428) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "items", "users"
+  add_foreign_key "user_details", "users"
 end
