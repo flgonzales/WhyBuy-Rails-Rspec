25f03895a2510038bb6fc9789c6f54e979b8c6a6
Fabiano Gonzalez <fabianolgonzales@gmail.com>
2016-07-20 17:26:02 +0200

    user spec rollback to previous tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe User, type: :model do
   describe "validations" do
-    it { is_expected.to have_many(:items) }
-    it { is_expected.to have_one(:user_detail) }
+      it { is_expected.to have_many(:items) }
+      it { is_expected.to have_one(:user_detail) }
   end
 end
