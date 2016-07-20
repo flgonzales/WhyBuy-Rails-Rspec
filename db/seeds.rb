5a46775f7f0823cdad9dfa0ee4cd630ea49bd1b1
Fabiano Gonzalez <fabianolgonzales@gmail.com>
2016-07-20 14:01:45 +0200

    items controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,10 @@ mike = User.create(email: "mike@mail.com",password: "password",)
 Item.destroy_all
 
 Item.create(name: "Drill", description: "Very nice electric Makita drill, you can use for all the small construction tasks in and around the house", available: true, start_avail: Date.today(), user_id: mike.id)
-
+Item.create(name: "Wheelbarrow", description: "Brand new wheelbarrow, very nice to do all your heavy lifting", available: true, start_avail: Date.today(), user_id: mike.id)
+Item.create(name: "Shovel", description: "Beautifull iron shovel haven't been used yet, so if you need to dig a hole this shovel won't let you down", available: false, start_avail: Date.today(), user_id: mike.id)
+Item.create(name: "Iron man suit", description: "The amazing Iron Man suit, known from the movies. Build for lightweight titanium and very user friendly", available: true, start_avail: Date.today(), user_id: mike.id)
+Item.create(name: "Rope", description: "Very strong, almost brand new robe. You can use it for moving", available: false, start_avail: Date.today(), user_id: mike.id)
 
 UserDetail.delete_all
 
