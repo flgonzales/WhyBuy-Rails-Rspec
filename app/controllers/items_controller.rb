2a00ada159c2ada90e5efc43576786b3241b4b06
Fabiano Gonzalez <fabianolgonzales@gmail.com>
2016-07-26 15:23:08 +0200

    controller added with Upd, Destroy and private methods

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,30 @@ class ItemsController < ApplicationController
 before_action :authenticate_user!
   def index
     @items = Item.order(name: :asc)
+
   end
 
   def show
     @item = Item.find( params[:id] )
   end
+
+  def update
+    @item.update(item_params)
+    redirect_to @item
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to items_path, notice: 'Item was removed with sucess.'
+  end
+
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:)
 end
