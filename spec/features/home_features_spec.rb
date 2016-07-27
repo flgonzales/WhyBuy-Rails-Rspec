681f4e9461c0d856050fcfec54ee4049ecc64453
Fabiano Gonzalez <fabianolgonzales@gmail.com>
2016-07-27 12:30:54 +0200

    feature tests

---
+++ spec/features/home_features_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+require 'support/items_steps'
+
+RSpec.describe "The whybuy homepage", :type => :feature do
+  include ItemsSteps
+
+  it "shows me the completed items header" do
+    visit_homepage
+    i_should_see_the_available_items_header
+  end
+
+  context "with a available item"
+    before do
+      visit_item_form
+      create_complete_items_with_description("test1")
+    end
+
+    it "shows the available items, in the available items list" do
+      visit_homepage
+      i_should_see_the_available_items_header("test1")
+    end
+  end
+end
---
+++ spec/support/items_steps.rb
@@ -0,0 +1,31 @@
+module ItemsSteps
+  def visit_item_form
+    visit("/items/new")
+  end
+
+  def create_complete_items_with_description(description)
+    check("item_available")
+    create_incomplete_items_with_description(desdescription)
+  end
+
+  def create_incomplete_items_with_description(description)
+    fill_in("item_available", :with => available )
+    click_button("Create Item")
+  end
+
+  def i_should_see_the_available_items_header
+    expect(page).to have_content("Available Items")
+  end
+
+  def i_should_see_the_unavailable_items_header
+    expect(page).to have_content("Your Unavailable Items")
+  end
+
+  def i_should_see_a_available_item_with_description(ddescription)
+    expect(page.find("#available_items")).to have_content(description)
+  end
+
+  def visit_homepage
+    visit("/")
+  end
+end
