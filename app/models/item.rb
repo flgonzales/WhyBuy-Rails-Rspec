c1381a587d835a0411424270752219d58d35bd7b
Fabiano Gonzalez <fabianolgonzales@gmail.com>
2016-07-28 09:41:09 +0200

    new item created

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,5 @@ class Item < ActiveRecord::Base
 
   validates :name, presence: true
   validates :description, presence: true
-  validates :user_id, presence: true
+  #validates :user_id, presence: true
 end
